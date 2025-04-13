090cbf64bd851fd469188080252bfe8137969444
Rick van Buuren <rdvanbuuren@gmail.com>
2025-04-13 15:58:05 +0200

    cleanup install script

--- install.sh
+++ install.sh
@@ -12,12 +12,8 @@ chmod 744 $SCRIPT_DIR/uninstall.sh
 chmod a+x $SCRIPT_DIR/service/run
 chmod 755 $SCRIPT_DIR/service/run
 
-
-
 # create sym-link to run script in deamon
 ln -s $SCRIPT_DIR/service /service/$DAEMON_NAME
-
-
 
 # add install-script to rc.local to be ready for firmware update
 filename=/data/rc.local
